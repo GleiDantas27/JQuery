b5bd3b3503d00ec83a5fd8ba2f252ccde0dd7d80
GleiDantas27 <47236163+GleiDantas27@users.noreply.github.com>
2019-05-11 20:01:27 -0700

    Add files via upload

---
+++ Js/Tmenu.js
@@ -0,0 +1,16 @@
+
+/*eslint-env browser*/
+
+$(document).ready(function () {
+    "use strict";
+	$('#productDescrip p:not(:first)').hide();
+	
+	$('#productNav li').click(function (e) {
+		$('#productDescrip p').hide();
+		$('#productNav .current').removeClass("current");
+		$(this).addClass('current');
+		var clicked = $(this).find('a:first').attr('href');
+		$('#productDescrip ' + clicked).fadeIn('fast');
+		e.preventDefault();
+	}).eq(0).addClass('current');
+});
