91fce039b32bd484ac081bfc1a823956d12cbfec
GleiDantas27 <47236163+GleiDantas27@users.noreply.github.com>
2019-05-11 18:24:47 -0700

    Add files via upload

---
+++ Js/accordion.js
@@ -0,0 +1,19 @@
+/*eslint-env browser*/
+
+$(document).ready(function(){
+
+	$('#accordionContent ul > li ul').hide();
+	
+	$('#accordionContent ul > li').click(function(){
+		var selfClick = $(this).find('ul:first').is(':visible');
+        
+		if(!selfClick){
+			$(this).parent().find('>li ul:visible').slideToggle();
+            $(this).children(".plusminus").text('-');
+		}else {
+             $(this).children(".plusminus").text('+');
+        }
+		$(this).find('ul:first').slideToggle();
+        
+	});
+});
